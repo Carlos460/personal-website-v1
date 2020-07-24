7cfeb4916b900a2a5214b6d86cf282ef4e436d07
Carlos Flores <carlosfloresalex@gmail.com>
2020-07-23 22:20:44 -0700

    project card hover animation

--- pages/components/portfolio.tsx
+++ pages/components/portfolio.tsx
@@ -1,18 +1,32 @@
+import ProjectCard from './project-card';
+
 export default function Portfoliio() {
   return (
     <div>
       <div className="container" id="portfolio">
         <h1>Portfolio</h1>
+        <div className="project-holder">
+          <ProjectCard title="task-it"></ProjectCard>
+          <ProjectCard title="moon-base"></ProjectCard>
+          <ProjectCard title="personal-website"></ProjectCard>
+        </div>
       </div>
       <style jsx>{`
         .container {
-          min-height: 50rem;
           background-color: #eeeeee;
           text-align: center;
-          padding: 1rem 2rem;
+          padding: 5rem 2rem;
+          height: auto;
+        }
+        .project-holder{
+          margin: 2rem 0rem;
+          display: flex;
+          justify-content: center;
+          flex-wrap: wrap;
+          height: auto;
         }
         h1 {
-          color: white;
+          color: black;
         }
       `}</style>
     </div>
--- pages/components/project-card.tsx
+++ pages/components/project-card.tsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from "react";
+
 export default function ProjectCard(props) {
+  const [cick, clicked] = useState(false);
+
+  useEffect(() => {});
   return (
     <div>
-      <h1>{props.page}</h1>
+      <div className="project-card">
+        <h1>{props.title}</h1>
+      </div>
+    <style>{`
+    .project-card{
+      background-color: #327D9D;
+      border-radius: 0.2rem;
+      margin: 1rem;
+      padding: 1rem;
+      height: 15rem;
+      width: 25rem;
+      transition: transform 0.2s ease;
+    }
+    .project-card:hover {
+      cursor: pointer;
+      transform: scale(1.1);
+    }
+    
+    `}</style>
     </div>
   );
 }
