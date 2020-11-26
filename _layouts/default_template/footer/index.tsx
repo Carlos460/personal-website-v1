df22ca34d765f1d768179ce343600286eb46c995
Carlos Flores <carlosfloresalex@gmail.com>
2020-11-26 14:31:27 -0500

    tweaked footer spacing

--- _layouts/default_template/footer/index.tsx
+++ _layouts/default_template/footer/index.tsx
@@ -8,8 +8,12 @@ export default function Footer() {
     <div>
       <footer>
         <section className="footer-title">
-          <h1>Thank you for your time, feel free to get in contact <Link href='/'><FooterLink><u>here</u></FooterLink></Link></h1>
+          <h1> <Link href='/'><FooterLink><u>Go to the Top</u></FooterLink></Link></h1>
           <br />
+          <div className="links-conatainer">
+            <br />
+            <h4>Email: carlosfloresalex@gmail.com</h4>
+          </div>
           <div className='links-container'>
             <FooterLink href='https://github.com/Carlos460' target='_blank'><b>Github</b></FooterLink>
             <FooterLink href='https://www.linkedin.com/in/carlos-flores-1795981b6/' target='_blank'><b>Linkin</b></FooterLink>
--- _layouts/default_template/footer/styles.js
+++ _layouts/default_template/footer/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const FooterLink = styled.a`
   color: white;
-  margin: 0.2rem 0rem;
+  margin: 0.2rem 1rem;
   text-decoration: none;
   &:hover {
     cursor: pointer;
